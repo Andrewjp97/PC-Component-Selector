5dcb3add7abf357b66def581b290417aa728a9a6
Andrew Paterson <Andrewjp97@me.com>
2024-07-14 09:25:16 -0400

    Initial Commit

---
+++ PC Component Selector/PC_Component_SelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PC_Component_SelectorApp.swift
+//  PC Component Selector
+//
+//  Created by Andrew Paterson on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct PC_Component_SelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
