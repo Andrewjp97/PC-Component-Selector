57d57be2e7a8b1937f836bb38f32576a6d4538ed
Andrew Jacob Paterson <Andrewjp97@me.com>
2024-07-16 09:15:39 -0400

    New RAM model implemented with rough UI

---
+++ PC Component Selector/Views/Extracted Views/ConditionalComponentSpecification.swift
@@ -0,0 +1,18 @@
+//
+//  ConditionalComponentSpecification.swift
+//  PC Component Selector
+//
+//  Created by Andrew Paterson on 7/16/24.
+//
+
+import SwiftUI
+
+struct ConditionalComponentSpecification: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConditionalComponentSpecification()
+}
